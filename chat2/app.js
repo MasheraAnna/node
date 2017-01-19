9b145ab6d018ffff1387b08a22c5bea3234ba4c4
Anna Mashera <anna.mashera@gmail.com>
2017-01-19 18:35:46 +0300

    sessions

--- chat2/app.js
+++ chat2/app.js
@@ -1,10 +1,17 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
 var http = require ('http');
 var path = require('path');
 var config = require('config');
 var log = require('./libs/log')(module);
+
 var bodyParser = require('body-parser');
+var cookieSession = require('cookie-session');
+
 var HttpError = require('./error').HttpError;
+var mongoose = require('./libs/mongoose');
 
 var app = express();
 app.set('port', config.get('port'));
@@ -17,8 +24,29 @@ app.set('view engine', "ejs");
 
 
 // middlewares
+
+
 app.use(bodyParser.raw());
+
 app.use(express.static(path.join(__dirname, "public")));
+
+var MongoDBStore = require('connect-mongodb')(session);
+var store = new MongoDBStore(
+	{
+
+	});
+
+app.use(cookieSession({
+	secret: config.get('session:secret'),
+	name: config.get('session:name'),
+	options: config.get('session:options'),
+	store: store
+}));
+
+app.use(function(req, res, next){
+	req.session.numberOfVisits = req.session.numberOfVisits + 1 || 1; 
+	res.send("Visits:" + req.session.numberOfVisits);
+});
 
 app.use(require('./middleware/sendHttpError'))
 
